494406b4b75214deb7c4af7a8b7380b9edb8358b
samszo <samszon@gmail.com>
2024-06-10 11:16:04 +0200

    url prod

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ a.getUser(u=>{
 //text generator parameters
 let urlMoteur = 'https://gestion.generateur.art/apiRead.php',
     //urlMoteur = 'http://localhost/generateur/apiRead.php',
-    m = new moteur({'apiUrl':'http://localhost/generateur/apiRead.php','id_dico':169,'id_oeu':72}),
+    m = new moteur({'apiUrl':urlMoteur,'id_dico':169,'id_oeu':72}),
     genPrompt = "[prompt_scene]";
 
 //image generator parameter
